8c29257c262cd1c2b0fad6ff54ee928ad78cb584
Brett Snyder <basnyder123@gmail.com>
2020-06-24 16:23:15 -0400

    daily challenge 6/24

---
+++ src/CountCompleteTreeNodes.java
@@ -0,0 +1,28 @@
+/*
+Runtime: 0 ms, faster than 100.00% of Java online submissions for String Matching in an Array.
+Memory Usage: 41.5 MB, less than 99.15% of Java online submissions for Count Complete Tree Nodes.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+}
---
+++ src/UniqueBinarySearchTrees.java
@@ -0,0 +1,30 @@
+/*
+Runtime: 0 ms, faster than 100.00% of Java online submissions for String Matching in an Array.
+Memory Usage: 36.1 MB, less than 59.70% of Java online submissions for Unique Binary Search Trees.
+*/
+
+class Solution {
+    //With Dynamic Programming
+    List<Integer> trees = new ArrayList<>();
+    public int numTrees(int n) {
+        if (n == 1) {
+            if (trees.isEmpty()) {
+                trees.add(1);
+            }
+            return 1;
+        }
+        if (n >= trees.size()) {
+            trees.add((int) (numTrees(n - 1) * (4.0 * n - 2) / (n + 1)));
+        }
+        return trees.get(n - 1);
+    }
+
+    //With Recursion
+    public int numTrees(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return (int) (numTrees(n - 1) * (4.0 * n - 2) / (n + 1));
+    }
+}
+
