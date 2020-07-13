8a50df76d44798a54234058853d8d3b0ea54afab
Brett Snyder <basnyder123@gmail.com>
2020-07-13 14:07:34 -0400

    dailychallenge 7/13

---
+++ src/SameTree.java
@@ -0,0 +1,28 @@
+/*
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Same Tree.
+Memory Usage: 36.9 MB, less than 60.15% of Java online submissions for Same Tree.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null || q == null) {
+            return p == q;
+        }
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
